53694f2e1f10489a24b7a5e44ec25e424ba5a96a
Sang <sangnn2012@gmail.com>
2023-03-05 11:21:25 +0700

    Add index page to month challenges project

--- challenges/views.py
+++ challenges/views.py
@@ -18,6 +18,18 @@ monthly_challenges = {
     "december": "Nahh",
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("monthly_challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     
@@ -31,6 +43,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('This month is not supported')
+        return HttpResponseNotFound('<h1>This month is not supported</h1>')
